fabad7ddb48f5940919d99885e5d35b0d3937e3b
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-06-27 16:05:34 +0530

    Added User Models

--- auth1/models.py
+++ auth1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=200)
+    key=models.TextField()
+    role=models.Choices('company','volunteer')
+
+    def __str__(self):
+        return self.email
+    
