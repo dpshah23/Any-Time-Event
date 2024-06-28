0e321f88d4b366e0f9637ee345085b7ec7cecb44
het2286 <hetshah2286@gmail.com>
2024-06-28 15:16:32 +0530

    Added Models to admin panel

--- auth1/admin.py
+++ auth1/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import users , otps
+from .models import *
 
 # Register your models here.
 
 admin.site.register(users)
 admin.site.register(otps)
+admin.site.register(company)
+admin.site.register(volunteer)
+admin.site.register(resetpass)
