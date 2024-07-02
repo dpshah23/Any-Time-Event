8dc9108e66bc060e7334df03391158cd7619b2cb
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-02 14:16:03 +0530

    Register Models Company

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(event)
+admin.site.register(reg_vol)
 
 # Register your models here.
--- company/models.py
+++ company/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class event(models.Model):
-    event_name = models.CharField(max_length=100)
+    event_company = models.CharField(max_length=200,null=True)
+    event_name = models.CharField(max_length=200)
     event_date = models.DateField()
     event_time = models.TimeField()
     event_location = models.CharField(max_length=100)
@@ -10,11 +11,13 @@ class event(models.Model):
     event_skills = models.CharField(max_length=200)
     security_deposite = models.IntegerField()
     event_vol = models.IntegerField()
+    event_completed = models.BooleanField(default=False)
     
     def __str__(self):
         return self.event_name
     
 class reg_vol(models.Model):
+    company_name = models.CharField(max_length=200,null=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=100)
