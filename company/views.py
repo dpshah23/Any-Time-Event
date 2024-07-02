f39b6d7fdb28592bdcbe612c520e5c0bcc74687f
het2286 <hetshah2286@gmail.com>
2024-07-02 16:08:59 +0530

    Updated views

--- company/views.py
+++ company/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django_ratelimit.decorators import ratelimit
 from auth1.models import company
-
+from .models import *
 # Create your views here.
 
 @ratelimit(key='ip', rate='5/m')
@@ -29,5 +29,8 @@ def add_event(request):
         event_mrp = request.POST.get('ratePerPerson')
         # event_completed =
         actual_amount = (event_mrp * 25)/100
+
+        event1 = event(event_company=event_company,event_name=event_name,event_date=event_date,event_time=event_time,event_location=event_location,event_loc_link=event_loc_link,event_description=event_description,event_skills=event_skills,event_rep=event_rep,event_rep_no=event_rep_no,event_mrp=event_mrp,actual_amount=actual_amount)
+        event1.save()
 
 # def get_vol(request):
