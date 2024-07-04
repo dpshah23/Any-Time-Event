b6d115c6b2b64edddb90e9081d15497814944bcc
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-04 15:19:17 +0530

    changed database

--- anytimeevent/settings.py
+++ anytimeevent/settings.py
@@ -134,7 +134,7 @@ load_dotenv()
 import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'defaultdb',      # Replace with your database name
         'USER': 'avnadmin',      # Replace with your database user
         'PASSWORD': os.getenv('PASSWORD'),  # Replace with your database password
